7a4e16fe2c0bcbfed3771756515c6084cb6f2aac
edne <edne@gmx.com>
2018-01-08 00:07:30 +0100

    Great macro

--- src/gg.h
+++ src/gg.h
@@ -7,3 +7,45 @@
 using namespace std;
 
 extern vector<function<void(void)>> actions;
+
+#define entity_register(name, init, action) extern "C" {\
+    static t_class *name##_class;\
+\
+    void name##_float(name *x, float _action_id) {\
+        x->input_action_id = (int)_action_id;\
+        outlet_float(x->outlet, x->action_id);\
+    }\
+\
+    void name##_bang(name *x) {\
+        outlet_float(x->outlet, x->action_id);\
+    }\
+\
+    void name##_free(name *x) {\
+        outlet_free(x->outlet);\
+    }\
+\
+    void *name##_new(void) {\
+        name *x = (name *)pd_new(name##_class);\
+        x->outlet = outlet_new(&x->x_obj, &s_pointer);\
+\
+        init(x);\
+\
+        x->action_id = actions.size();\
+        actions.push_back([x](){action(x);});\
+        x->input_action_id = -1;\
+\
+        return (void *)x;\
+    }\
+\
+    void name##_setup(void) {\
+        name##_class = class_new(\
+            gensym(#name),\
+            (t_newmethod) name##_new,\
+            (t_method) name##_free,\
+            sizeof(name),\
+            CLASS_DEFAULT, A_NULL\
+        );\
+        class_addbang(name##_class, name##_bang);\
+        class_addfloat(name##_class, name##_float);\
+    }\
+}
--- src/gg_scale.cpp
+++ src/gg_scale.cpp
@@ -1,58 +1,30 @@
 #include <gg.h>
 
-static t_class *gg_scale_class;
-
 typedef struct _gg_scale {
     t_object x_obj;
     t_outlet *outlet;
     int action_id;
     int input_action_id;
+
     float ratio;
-} t_gg_scale;
+} gg_scale;
 
-void gg_scale_float(t_gg_scale *x, float _action_id) {
-    x->input_action_id = (int)_action_id;
-    outlet_float(x->outlet, x->action_id);
-}
-
-void *gg_scale_new(void) {
-    t_gg_scale *x = (t_gg_scale *)pd_new(gg_scale_class);
-    x->outlet = outlet_new(&x->x_obj, &s_pointer);
-
+static void init(gg_scale *x) {
     x->ratio = 1;
     floatinlet_new(&x->x_obj, &x->ratio);
-
-    x->action_id = actions.size();
-
-    actions.push_back([x](){
-        int action_id = x->input_action_id;
-        float ratio = x->ratio;
-
-        glPushMatrix();
-        glScalef(ratio, ratio, ratio);
-
-        if(action_id >= 0 && action_id < (int)actions.size())
-            actions[action_id]();
-
-        glPopMatrix();
-    });
-
-    return (void *)x;
 }
 
-void gg_scale_free(t_gg_scale *x) {
-    outlet_free(x->outlet);
+static void action(gg_scale *x) {
+    int action_id = x->input_action_id;
+    float ratio = x->ratio;
+
+    glPushMatrix();
+    glScalef(ratio, ratio, ratio);
+
+    if(action_id >= 0 && action_id < (int)actions.size())
+        actions[action_id]();
+
+    glPopMatrix();
 }
 
-extern "C" {
-    void gg_scale_setup(void) {
-        gg_scale_class = class_new(
-            gensym("gg_scale"),
-            (t_newmethod)gg_scale_new,
-            (t_method)gg_scale_free,
-            sizeof(t_gg_scale),
-            CLASS_DEFAULT, A_NULL
-        );
-        class_addfloat(gg_scale_class, gg_scale_float);
-    }
-}
+entity_register(gg_scale, init, action);
